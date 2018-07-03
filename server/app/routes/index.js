01e59e3b013555371ac3003658b219165ef0de69
Ansa1995 <ansaps95@gmail.com>
2018-07-03 15:50:22 +0530

    changes in es lint configuration

--- server/app/routes/index.js
+++ server/app/routes/index.js
@@ -1,29 +1,25 @@
 // routes/index.js
-const controller = require('../controllers/cntrlr.js');
+const controller = require('../controllers/cntrlr.js')
 
-module.exports = function application(app) {
-//  defining the route for user signup---
-app.post('/api/signup', async function(req, res,next){
+module.exports = function (app) {
+ // ---defining the route for user signup---
+ app.post('/api/signup', async function(req, res,next) {
    controller.signup(req,res,next)
  }),
- //---defining the route for user signup---
+ // ---defining the route for user signup---
  app.post('/api/signin',async function(req,res,next){
    controller.signin(req,res,next)
  }),
- //---defining the route for adding video---
+ // ---defining the route for adding video---
  app.post('/api/add',async function(req,res,next){
    controller.add(req, res, next)
    
  }),
- //---defining the route for listing videos of pariticular users---
+ // ---defining the route for listing videos of pariticular users---
  app.post('/api/userlist', async function(req, res, next){
    controller.user_list(req, res, next)
  })
  app.post('/api/edit',async function(req,res,next){
   //console.log("post received")
- })
-
- app.get('/api/search',async function(req,res,next){
-   controller.serach(req,res,next)
  })
 }
--- server/app/services/user_todo.js
+++ server/app/services/user_todo.js
@@ -1,64 +1,52 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/users.js');
+const video = require('../models/video.js');
+const util = require('../utiles/utils_enc.js');
 
-const user = require('../models/users.js')
-const video = require('../models/video.js')
-const util = require('../utiles/utils_enc.js')
-const jwt = require('jsonwebtoken')
 
 module.exports = {
 
-  //---defining the function for adding users to mlab---
-  user_insert: function(req, res, username,email,hash){
-    var userData = new user({
-      username: username,
-      email: email,
-      password: hash
-    })
-    user.findOne({email:userData.email},function(err,docs){
-      if(err){
-        res.json({"status":"400","msg":"db error"})
-      } 
-      else if(docs == null){
-        userData.save().then(
-          item => {
-            res.json({"status":"200","msg":"Success"})
-          }
-        )
-      
-      }else
-      {
-        res.json({"status":"400","msg":"User exist"})
-      }
-      
-    })
+  // defining the function for adding users to mlab
+  user_insert: (req, res, usernames, emails, hash) => {
+    const userData = new User({
+      username: usernames,
+      email: emails,
+      password: hash,
+    });
+
+    User.findOne({ email: userData.email }, (err, docs) => {
+      if (err) {
+        res.json({ status: '400', msg: 'db error' });
+      } else if (docs == null) {
+        userData.save().then(() => { res.json({ status: '200', msg: 'Success' }); });
+      } else { res.json({ status: '400', msg: 'User exist' }); }
+    });
   },
-    //---defining the function for signup by fetching values from  mlab---
-    user_signin:function(req,res,next,email,password){
-    
-      user.findOne({email:email},function (err,docs){
-      if (err){
-        res.json({"status":"error","msg":"db_error_found"})
-      } else if (docs==null){
-        res.json({"status":"error","msg":"no_user_found"})
-      } else{
-        var decrypt = util.decrypt(password,docs.password)
-        if (decrypt){
+  // defining the function for signup by fetching values from  mlab---
+  user_signin: (req, res, next, emails, password) => {
+    User.findOne({ email: emails }, (err, docs) => {
+      if (err) {
+        res.json({ status: '400', msg: 'db_error_found' });
+      } else if (docs == null) {
+        res.json({ status: 400, msg: 'no_user_found' });
+      } else {
+        const decrypt = util.decrypt(password, docs.password);
+        if (decrypt) {
           const JWTtoken = jwt.sign({
-            email: email
+            email: emails,
           },
           'secret',
           {
-            expiresIn: '2h'
-          })
-          
-          res.json({"status":"success","msg":JWTtoken})
-          
+            expiresIn: '2h',
+          });
+          res.json({ status: '200', msg: JWTtoken });
         } else {
-          res.json({"status":"error","msg":"password not matching"})
+          res.json({ status: '400', msg: 'password not matching' });
         }
       }
-    })
+    });
   },
-  //---defining the function for adding video on mlab---
+  // defining the function for adding video on mlab---
     video_add: (req, res, next, token, title, videourl, description, key) => {
       jwt.verify(token, 'secret', (err, decoded) => {
         if (err) {
@@ -97,51 +85,34 @@ module.exports = {
                     })
                   }
                 }
-              )
-            }
+            })
+        }
+    });
+  },
+  // defining the function for listing  videos of particular users on mlab
+  user_list: async (req, res, next, token) => {
+    jwt.verify(token, 'secret', (err, decoded) => {
+      if (err) {
+        res.json({ status: 400, msg: 'token authentication failed' });
+      } else {
+        User.findOne({ email: decoded.email }, async (errs, docs) => {
+          if (errs) {
+            res.json({ status: '400', msg: 'db error' });
+          } else if (docs == null) {
+            res.json({ status: '400', msg: 'no such user found' });
+          } else {
+            video.find({ user_id: docs._id }, { videourl: 1, _id: 0 }, (err1, docs1) => {
+              if (err1) {
+                res.json({ status: '400', msg: 'db error' });
+              } else if (docs1 == null) {
+                res.json({ status: '400', msg: 'No videos added by this user' });
+              } else {
+                res.json({ status: '200', msg: docs1 });
+              }
+            });
           }
-        )
-      },
-  //---defining the function for listing  videos of particular users on mlab---
-  user_list:async function(req, res, next, token){
-
-    jwt.verify(token,'secret',function(err,decoded){
-      if(err)
-      {
-          res.json({'status':'400','msg':'token authentication failed'})
+        });
       }
-      else
-      {
-          user.findOne({email:decoded.email},async function(err,docs){
-              if(err)
-              {
-                  res.json({"status":"400","msg":"db error"})
-              }
-              else if(docs==null){
-                  res.json({'status':'400','msg':'no such user found'})
-              }
-              else
-              {
-                  video.find({user_id:docs._id},{videourl:1,_id:0},function(err,docs1){
-                    if(err)
-                    {
-                      res.json({"status":"400","msg":"db error"})
-                    }
-                    else if(docs1==null)
-                    {
-                      res.json({"status":"400","msg":"No videos added by this user"})
-                    }
-                    else
-                    {
-                      var urls = docs1
-                      res.json({"status":"200","msg":urls})
-                    }
-                  })
-                  
-              }
-          })
-      }
-  })
-
-  }
-}
+    });
+  },
+};
